45e599561432d0c054001e637fcc01ff19abec31
easytoget <yide1986@gmail.com>
2021-05-26 23:14:50 +0800

    建立專案資料

---
+++ js/admin.js
@@ -0,0 +1,86 @@
+const url = 'https://vue3-course-api.hexschool.io';
+const path = 'easytoget';
+
+const app = {
+  data: {
+    products: [],
+  },
+  getData() {
+    axios.get(`${url}/api/${path}/admin/products`)
+      .then((res) => {
+        console.log(res);
+        if (res.data.success) {
+          this.data.products = res.data.products;
+          this.render();
+        }
+      })
+  },
+  render() {
+    const productListDom = document.querySelector('#productList');
+    const productCountDom = document.querySelector('#productCount');
+    // let template = '';
+    // this.data.products.forEach((item) => {
+    //   template = template + `
+    //     <tr>
+    //       <td>${item.title}</td>
+    //       <td width="120">
+    //         ${item.origin_price}
+    //       </td>
+    //       <td width="120">
+    //         ${item.price}
+    //       </td>
+    //       <td width="100">
+    //         <span class="">${item.is_enabled}</span>
+    //       </td>
+    //       <td width="120">
+    //         <button type="button" class="btn btn-sm btn-outline-danger move deleteBtn" data-action="remove"
+    //           data-id=""> 刪除 </button>
+    //       </td>
+    //     </tr>`;
+    // })
+    const template = this.data.products.map(item => `
+      <tr>
+        <td>${item.title}</td>
+        <td width="120">
+          ${item.origin_price}
+        </td>
+        <td width="120">
+          ${item.price}
+        </td>
+        <td width="100">
+          <span class="">${item.is_enabled}</span>
+        </td>
+        <td width="120">
+          <button type="button" class="btn btn-sm btn-outline-danger move deleteBtn" data-action="remove"
+            data-id="${item.id}"> 刪除 </button>
+        </td>
+      </tr>
+    `).join('');
+    productListDom.innerHTML = template;
+    productCountDom.textContent = this.data.products.length;
+
+    const deleteBtns = document.querySelectorAll('.deleteBtn');
+    deleteBtns.forEach(btn => {
+      btn.addEventListener('click', this.deleteProduct)
+    })
+  },
+  deleteProduct(evt) {
+    const id = evt.target.dataset.id;
+    axios.delete(`${url}/api/${path}/admin/product/${id}`)
+      .then(res => {
+        console.log(res);
+        app.getData();
+      })
+  },
+  init() {
+    // Cookie 取出
+    const token = document.cookie.replace(/(?:(?:^|.*;\s*)hexToken\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+    axios.defaults.headers.common['Authorization'] = token;
+    // axios.post(`${url}/api/user/check`) 
+    //   .then((res) => {
+    //     console.log(res);
+    //   })
+    this.getData();
+  }
+}
+app.init();
---
+++ js/all.js
@@ -0,0 +1,86 @@
+const url = 'https://vue3-course-api.hexschool.io';
+
+const usernameInput = document.querySelector('#username');
+const passwordInput = document.querySelector('#password');
+const loginBtn = document.querySelector('.js-btn');
+
+loginBtn.addEventListener('click', login);
+function login() {
+  const username = usernameInput.value;
+  const password = passwordInput.value;
+  const loginData = {
+    username,
+    password,
+  };
+  axios.post(`${url}/admin/signin`, loginData) //發出請求
+    .then((res) => {
+      console.log(res);
+      if (res.data.success) {
+        const { token, expired } = res.data;
+        document.cookie = `hexToken=${token}; expires=${new Date(expired)}; path=/`; // cookie 存入
+        window.location = 'admin.html';
+      }
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const url = 'https://vue3-course-api.hexschool.io';
+// const path = 'easytoget';
+
+// const usernameInput = document.querySelector('#username');
+// const passwordInput = document.querySelector('#password');
+// const loginBtn = document.querySelector('.js-btn');
+
+// loginBtn.addEventListener('click', login);
+
+// function login() {
+//   const username = usernameInput.value;
+//   const password = passwordInput.value;
+//   const loginData = {
+//     username,
+//     password
+//   };
+//   axios.post(`${url}/admin/signin`, loginData) //發出請求
+//     .then((res) => {
+//       console.log(res);
+//       if (res.data.success) {
+//         // const token = res.data.token;
+//         // const expired = res.data.expired;
+//         const { token, expired } = res.data;
+//         document.cookie = `hexToken=${token}; expires=${new Date(expired)}; path=/`;
+//       }
+//     });
+
+// }
+
+// const app = {
+//   data: {
+
+//   },
+//   init() {
+//     const token = document.cookie.replace(/(?:(?:^|.*;\s*)hexToken\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+//     axios.defaults.headers.common['Authorization'] = token;
+//     // axios.post(`${url}/api/user/check`)
+//     //   .then(res => {
+//     //     console.log(res);
+//     //   })
+//   }
+// }
+// app.init();
