4ae8a5ec12116464e70d639b276f2f0454f5a017
easytoget <yide1986@gmail.com>
2021-05-30 03:39:22 +0800

    login 頁面，修改成 Vue3

--- js/all.js
+++ js/all.js
@@ -1,32 +1,70 @@
-const url = 'https://vue3-course-api.hexschool.io';
+import { createApp } from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.0.9/vue.esm-browser.js';
 
-const usernameInput = document.querySelector('#username');
-const passwordInput = document.querySelector('#password');
-const loginBtn = document.querySelector('.js-btn');
+createApp({
+  data() {
+    return {
+      apiUrl: 'https://vue3-course-api.hexschool.io',
+      user: {
+        username: '',
+        password: '',
+      },
+    };
+  },
+  methods: {
+    login() {
+      const api = `${this.apiUrl}/admin/signin`;
+      axios.post(api, this.user)
+        .then((res) => {
+          if (res.data.success) {
+            const { token, expired } = res.data;
+            // 寫入 cookie token
+            // expires 設置有效時間
+            document.cookie = `hexToken=${token}; expires=${new Date(expired)}; path=/`;
+            window.location = 'admin.html';
+          } else {
+            alert(res.data.message);
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        })
+    },
+  },
+}).mount('#app');
 
-loginBtn.addEventListener('click', login);
-function login() {
-  const username = usernameInput.value;
-  const password = passwordInput.value;
-  const loginData = {
-    username,
-    password,
-  };
-  axios.post(`${url}/admin/signin`, loginData) //發出請求
-    .then((res) => {
-      console.log(res);
-      if (res.data.success) {
-        const { token, expired } = res.data;
-        document.cookie = `hexToken=${token}; expires=${new Date(expired)}; path=/`; // cookie 存入
-        window.location = 'admin.html';
-      }else {
-        alert("登入未成功，請輸入正確帳號密碼");
-      }
-    })
-    .catch((err) => {
-      console.log(err);
-    })
-}
+
+
+// const url = 'https://vue3-course-api.hexschool.io';
+
+// const usernameInput = document.querySelector('#username');
+// const passwordInput = document.querySelector('#password');
+// const loginBtn = document.querySelector('.js-btn');
+
+// loginBtn.addEventListener('click', login);
+// function login() {
+//   const username = usernameInput.value;
+//   const password = passwordInput.value;
+//   const loginData = {
+//     username,
+//     password,
+//   };
+//   axios.post(`${url}/admin/signin`, loginData) //發出請求
+//     .then((res) => {
+//       console.log(res);
+//       if (res.data.success) {
+//         const { token, expired } = res.data;
+//         // 寫入 cookie token
+//           // expires 設置有效時間
+//         document.cookie = `hexToken=${token}; expires=${new Date(expired)}; path=/`;
+//         window.location = 'admin.html';
+//       }else {
+//         alert("登入未成功，請輸入正確帳號密碼");
+//       }
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     })
+// }
 
 
 
